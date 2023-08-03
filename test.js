e71945ab7bf462409d514aee6c1c2e319ff39607
hawkdawg <113959779+jlsude@users.noreply.github.com>
2023-08-03 19:42:33 +0800

    fix: cleaned the test js file

--- test.js
+++ test.js
@@ -1,7 +1,1 @@
 console.log('test.js')
-console.log("added github workflow I hope it works")
-console.log("added feat 1")
-
-console.log("added feat 2")
-console.log("added fix 1")
-console.log("added feat 2")
